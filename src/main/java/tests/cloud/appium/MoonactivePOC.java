45d061d6826abe1bd90d9d5fb79d406d24d3b545
MichaelBaran <michael.baran@experitest.com>
2020-12-20 17:00:46 +0200

    updated poc test

--- src/main/java/tests/cloud/appium/MoonactivePOC.java
+++ src/main/java/tests/cloud/appium/MoonactivePOC.java
@@ -11,11 +11,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import tests.BaseTest;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
@@ -28,7 +31,8 @@ public class MoonactivePOC extends BaseTest {
     DesiredCapabilities dc = new DesiredCapabilities();
     HashMap<String, Integer> serialToPort = new HashMap<String, Integer>() {
         {
-            put("99161FFAZ007RY", 13001); put("ce12160cf49a1a2104", 13002);
+//            put("99161FFAZ007RY", 13001); put("ce12160cf49a1a2104", 13002);
+            put("668bbfe5", 13001); put("031603c437ba1901", 13002);
         }
     };
 
@@ -38,8 +42,6 @@ public class MoonactivePOC extends BaseTest {
         dc.setCapability("testName", "POC");
         dc.setCapability("accessKey", this.accessKey);
         dc.setCapability("deviceQuery", "@serialNumber='" + this.deviceSN + "'");
-//        dc.setCapability(MobileCapabilityType.UDID, "09241FDD40079M");
-//        dc.setCapability();
         dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.echo_app/.MainActivity");
         dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.echo_app");
         dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".MainActivity");
@@ -53,18 +55,24 @@ public class MoonactivePOC extends BaseTest {
         do {
             String response = "";
             final String expectedResponse = String.valueOf(System.currentTimeMillis());
-//            try (Socket socket = new Socket("michaels-mini", 13001)) {
             try (Socket socket = new Socket("localhost", serialToPort.get(this.deviceSN))) {
                 socket.getOutputStream().write((expectedResponse + "\n").getBytes(StandardCharsets.UTF_8));
                 final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 response = reader.readLine();
+                int retries = 0;
+                while (response == null && retries < 2) {
+                    socket.getOutputStream().write((expectedResponse + "\n").getBytes(StandardCharsets.UTF_8));
+                    response = reader.readLine();
+                    retries++;
+                    Thread.sleep(100);
+                }
                 assertEquals(expectedResponse, response);
-                System.out.println(this.deviceSN + ": " + expectedResponse + ", " + response);
+                System.out.println(new Date() + ": " + this.deviceSN + ": " + expectedResponse + ", " + response);
+                Thread.sleep(100);
             } catch (Exception e) {
                 e.printStackTrace();
-//                System.out.println(MoonactivePOC.class.getName() + ": Report URL for device 09241FDD40079M: "+ driver.getCapabilities().getCapability("reportUrl"));
                 driver.quit();
-                fail("Connection error. Probably..");
+                fail(e.getMessage());
             }
         } while (System.currentTimeMillis() - start < TOTAL_TIME);
     }
